94ce6127f41a1240b23f59077f2aca9da2bbfb49
Mohamed KHOUS <khous.ayoub@gmail.com>
2024-12-17 18:02:04 +0000

    Refactor cache items/key + remove dynamic long/lat with request

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class WeatherController extends AbstractController
@@ -23,33 +24,34 @@ class WeatherController extends AbstractController
     #[Route('/weather', name: 'weather', methods: ['GET'])]
     public function getWeather(Request $request): JsonResponse
     {
-        // Get latitude and longitude from query parameters Default to Cancer Research London
-        $latitude = $request->query->get('latitude', 51.5196);
-        $longitude = $request->query->get('latitude', -0.1541);
+        // Create the cache key
+        $cacheKey = 'weather_data';
+        $cacheItem = $this->cache->getItem($cacheKey);
 
-        // Cache key with dynamic coordinates
-        $cacheKey = 'weather_data_'.$latitude.'_'.$longitude;
+        if (!$cacheItem->isHit()) {
+            // Fetch and cache the data from the Open-Meteo API endpoint
+            $weatherData = $this->cache->get($cacheItem, function (ItemInterface $item) {
+                // the result will disapear after 300 sec (5min)
+                $item->expiresAfter(300);
 
-        // Fetch and cache the data from the Open-Meteo API endpoint
-        $weatherData = $this->cache->get($cacheKey, function ($item, $latitude, $longitude) {
-            // the result will disapear after 300 sec (5min)
-            $item->expiresAfter(300);
-            
-            $url = "https://api.open-meteo.com/v1/forecast?latitude={$latitude}&longitude={$longitude}&current_weather=true&hourly=temperature_2m&forecast_days=1";
-            
-            // Error handling
-            try {
-                $response = $this->httpClient->request('GET', $url);
-            } catch (\Exception $e) {
-                return $this->json([
-                    'error' => 'Failed to fetch data from Open-Meteo API.',
-                    'message' => $e->getMessage(),
-                ], 500);
-            }
+                $url = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m&hourly=temperature_2m&forecast_days=1';
 
-            return $response->toArray();
-        });
+                // Error handling
+                try {
+                    $response = $this->httpClient->request('GET', $url);
+                } catch (\Exception $e) {
+                    return $this->json([
+                        'error' => 'Failed to fetch data from Open-Meteo API.',
+                        'message' => $e->getMessage(),
+                    ], 500);
+                }
 
-        return $this->json(['data' => $weatherData, 'status' => 200]);
+                return $response->toArray();
+            });
+
+            return $this->json(['data' => $weatherData, 'status' => 200]);
+        } else {
+            return $this->json(['data' => $cacheItem->get(), 'Hits' => 'Data fetched from Cache', 'status' => 200]);
+        }
     }
 }
